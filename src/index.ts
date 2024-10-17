ef85cb19deb24f3eb6914522b25db7e58df77986
fabri <fguespe@gmail.com>
2024-10-17 09:06:38 -0700

    update

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,29 @@ import { run, HandlerContext } from "@xmtp/message-kit";
 import { handler as agent } from "./handler/agent.js";
 import { handleSwap } from "./handler/swap.js";
 
-run(async (context: HandlerContext) => {
-  const {
-    message: {
-      typeId,
-      content: { content: text, command, params },
-    },
-  } = context;
+run(
+  async (context: HandlerContext) => {
+    const {
+      message: {
+        typeId,
+        content: { content: text, command, params },
+      },
+    } = context;
 
-  if (typeId !== "text") return;
+    if (typeId !== "text") return;
 
-  if (text.includes("@swap")) {
-    await agent(context);
-  } else if (
-    text.startsWith("/swap") &&
-    params.token_from &&
-    params.token_to &&
-    params.amount
-  ) {
-    await handleSwap(context);
+    if (text.includes("@swap")) {
+      await agent(context);
+    } else if (
+      text.startsWith("/swap") &&
+      params.token_from &&
+      params.token_to &&
+      params.amount
+    ) {
+      await handleSwap(context);
+    }
+  },
+  {
+    logging: process.env.NODE_ENV === "production" ? "debug" : "off",
   }
-});
+);
