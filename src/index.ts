2c8f5b8ab7c91d398f6433581211313acd41d335
fabri <fguespe@gmail.com>
2024-11-02 17:34:33 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import { run, HandlerContext } from "@xmtp/message-kit";
 import { baseTxAgent } from "./handler/base.js";
 
-run(async (context: HandlerContext) => {
-  /*All the commands are handled through the commands file*/
-  /* If its just text, it will be handled by the ensAgent*/
-  /* If its a group message, it will be handled by the groupAgent*/
+run(
+  async (context: HandlerContext) => {
+    /*All the commands are handled through the commands file*/
+    /* If its just text, it will be handled by the ensAgent*/
+    /* If its a group message, it will be handled by the groupAgent*/
 
-  baseTxAgent(context);
-}{
-  client:{
-    logging: process.env.NODE_ENV === "production" ? "debug" : "off",
+    baseTxAgent(context);
   },
-});
+  {
+    client: {
+      logging: process.env.NODE_ENV === "production" ? "debug" : "off",
+    },
+  }
+);
