6722f381d03394f43eae60070d2eadbff57b6c4d
fabri <fguespe@gmail.com>
2024-11-03 15:47:07 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,20 @@ import { getUserInfo } from "./lib/resolver.js";
 import { agent_prompt } from "./prompt.js";
 run(
   async (context: HandlerContext) => {
-    /*All the commands are handled through the commands file*/
-    /* If its just text, it will be handled by the ensAgent*/
-    /* If its a group message, it will be handled by the groupAgent*/
-    if (!process?.env?.OPEN_AI_API_KEY) {
-      console.log("No OPEN_AI_API_KEY found in .env");
-      return;
-    }
-
     const {
       message: {
         content: { content, params },
         sender,
       },
     } = context;
+
+    /*All the commands are handled through the commands file*/
+    /* If its just text, it will be handled by the ensAgent*/
+    /* If its a group message, it will be handled by the groupAgent*/
+    if (!process?.env?.OPEN_AI_API_KEY) {
+      console.warn("No OPEN_AI_API_KEY found in .env");
+      return;
+    }
 
     try {
       let userPrompt = params?.prompt ?? content;
